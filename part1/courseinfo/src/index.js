38aca65243a524bfcb3b5bb9c5a80f78a4f9ef70
mkessler <mikaelkessler@gmail.com>
2019-07-16 00:13:59 -0400

    Part 1a - Step 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,9 +7,19 @@ const Header = (props) => (
   </h1>
 )
 
-const Content = (props) => (
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1}/>
+      <Part name={props.part2} exercises={props.exercises2}/>
+      <Part name={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.name} {props.exercises}
   </p>
 )
 
@@ -31,9 +41,7 @@ const App = () => {
   return (
     <>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total count={exercises1 + exercises2 + exercises3}/>
     </>
   )
