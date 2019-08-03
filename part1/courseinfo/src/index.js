230f8c94b961f8d54a77df7f7a0743f878353eb4
mkessler <mikaelkessler@gmail.com>
2019-08-03 01:10:11 -0400

    Part 1d - Step 6'

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,34 +1,39 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <div>{counter}</div>
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
+const Header = ({text}) => (
+  <h1>{text}</h1>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
-  const setToValue = (value) => () => setCounter(value)
+const Statistic = ({text, value}) => (
+  <div>
+    {text} {value}
+  </div>
+)
 
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={setToValue(counter + 1)}
-        text='plus'
-      />
-      <Button
-        onClick={setToValue(counter - 1)}
-        text='minus'
-      />
-      <Button
-        onClick={setToValue(0)}
-        text='zero'
-      />
-    </div>
+    <>
+      <Header text="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics" />
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+    </>
   )
 }
 
