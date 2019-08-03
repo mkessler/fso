1d68d719eae8193eecaca9f40fd599c30241cb17
mkessler <mikaelkessler@gmail.com>
2019-08-03 03:01:40 -0400

    Part 1d - 1.11 Step 6

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,17 +12,42 @@ const Header = ({text}) => (
 )
 
 const Statistic = ({text, value}) => (
-  <div>
-    {text} {value}
-  </div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+
+  if (all > 0) {
+    return (
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={all} />
+          <Statistic text="average" value={(good - bad) / all} />
+          <Statistic text="positive" value={(good / all) * 100 + ' %'} />
+        </tbody>
+      </table>
+    )
+  } else {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + bad + neutral
 
   return (
     <>
@@ -31,12 +56,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={all} />
-      <Statistic text="average" value={(good - bad) / all} />
-      <Statistic text="positive" value={(good / all) * 100 + ' %'} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
