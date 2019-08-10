d1d95344c32110a9752a5fd78fa680ac0043b22c
mkessler <mikaelkessler@gmail.com>
2019-08-10 03:05:48 -0400

    Part 2.9: phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react'
 import Person from './components/Person'
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ])
+const App = (props) => {
+  const [ persons, setPersons] = useState(props.persons)
+  const [ newFilter, setNewFilter ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
 
@@ -26,20 +22,27 @@ const App = () => {
     }
   }
 
-  const handleNameChange = (event) => {
+  const handleFilterChange = (event) => {
     console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const existingPerson = () =>
     persons.find(person => person.name === newName)
 
-  const rows = () => persons.map(person =>
+  const personsToShow = newFilter
+    ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    : persons
+
+  const rows = () => personsToShow.map(person =>
     <Person
       key={person.name}
       person={person}
@@ -49,6 +52,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const persons = [
+  { name: 'Arto Hellas', number: '040-123456' },
+  { name: 'Ada Lovelace', number: '39-44-5323523' },
+  { name: 'Dan Abramov', number: '12-43-234345' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122' }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <App persons={persons} />,
+  document.getElementById('root')
+);
